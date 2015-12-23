dabe39a44f061557d23e28cab5710fab843ef0d9
zhangrenyang-t510 <zhang_renyang@>
2015-12-23 21:15:09 +0800

    init

--- day2/2.static.js
+++ day2/2.static.js
@@ -1,16 +1,17 @@
 var http = require('http');
+var fs = require('fs');
 /**
  *
  * @param req 请求对象 电话的听筒
  * @param res 响应对象 电话的话筒
  */
 function serve(req,res){
-    console.log(req.method);
-    console.log(req.url);
-    console.log(req.headers);
-    res.writeHead(200,{'Content-Type':'text/plain'})
-    res.write('<h1>hello'+new Date().toLocaleString()+'</h1>');//向客户端说话
-    res.end();//结束响应，完成请求，挂掉电话
+    // err 如果文件读取失败，会把原因放在err对象里
+    // data 如果读取成功了，会把文件的内容放在data对象里
+    fs.readFile('index.html',function(err,data){
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.end(data);
+    })
 }
 //当有连接到来的时候调用serve方法为客户端提供服务
 var server = http.createServer(serve);//创建一个服务
