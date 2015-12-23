5fd1e540478efd1ef4d4b3556e4a4dc8807799fa
zhangrenyang-t510 <zhang_renyang@>
2015-12-23 22:10:36 +0800

    init

--- day2/h5/uploader.js
+++ day2/h5/uploader.js
@@ -1,22 +1,38 @@
 (function(window){
  window.uploader = (function(){
+     var Promise = function(){
+         this.success = this.error = function(){}
+     }
+     Promise.prototype.then = function(success,error){
+         this.success = success;
+         this.error = error;
+     }
+     function Deferred(){
+         this.promise = new Promise();
+         this.status = 'init';
+     }
+     Deferred.prototype = {
+         resolve:function(data){
+             this.status = 'success';
+             this.promise.success(data);
+         },
+         reject:function(data){
+             this.status = 'fail';
+             this.promise.error(data);
+         }
+     }
      // url 上传的路径  id 文件域的ID
      return function(config){
+       var deferred = new Deferred();
        var xhr = new XMLHttpRequest();
        xhr.open('POST',config.url,true);
        xhr.onreadystatechange = function(){
            if(xhr.readyState == 4){
                var result = xhr.responseText;
                if(/2\d{2}/.test(xhr.status)){
-                   //成功
-                   var result = JSON.parse(result);
-                   var msg = result.msg;
-                   var img = document.createElement('img');
-                   img.src = msg;
-                   img.className = 'img-responsive';
-                   document.body.appendChild(img);
+                   deferred.resolve(result);
                }else{
-                   //失败
+                   deferred.error(result);
                }
            }
        }
@@ -24,6 +40,7 @@
        var formData = new FormData();
        formData.append('avatar',file);
        xhr.send(formData);
+       return deferred.promise;
      }
  })();
 })(window)
